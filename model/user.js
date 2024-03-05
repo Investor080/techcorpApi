6accaf5284c8de5b9ad4f2385f22ffd99c44e37f
unknown <alamoyoolayinka@gmail.com>
2024-03-05 13:36:03 +0000

    completed

--- model/user.js
+++ model/user.js
@@ -6,7 +6,9 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
-});
+},    
+     {timestamps:true},
+);
 
 
 const User = mongoose.model('User', userSchema);
